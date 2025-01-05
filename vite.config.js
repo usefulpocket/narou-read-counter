16215726e1c540e4dd3884e264a8a87004846c84
usefulpocket <usefulpocket@gmail.com>
2025-01-06 00:26:57 +0900

    refs #4 Vite+Svelte導入（Svelteの記述は使っていない）

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import copy from 'rollup-plugin-copy';
+
+export default defineConfig({
+  root: './options-src',
+  base: '/options',
+  build: {
+    outDir: '../options',
+    emptyOutDir: true
+  },
+  plugins: [
+    svelte(),
+    copy({
+      targets: [
+        { src: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js', dest: 'lib' },
+        { src: 'node_modules/webextension-polyfill/dist/browser-polyfill.min.js.map', dest: 'lib' },
+        { src: 'node_modules/milligram/dist/milligram.min.css', dest: 'lib' },
+        { src: 'node_modules/milligram/dist/milligram.min.css.map', dest: 'lib' },
+        { src: 'node_modules/idb/build/umd.js', dest: 'lib' }
+      ]
+    })
+  ],
+})
